68f5a78e5008ea51ad7cd97887bb130db3b6bce5
omotolani <omotolanionajoko@gmail.com>
2025-11-26 11:13:54 +0100

    chore(weather-icons):add weather icons paths

--- src/utils/WeatherIcon.ts
+++ src/utils/WeatherIcon.ts
@@ -1,4 +1,51 @@
 export const weatherIcons = {
+  // Clear / Sunny
   0: 'src/assets/images/icon-sunny.webp',
-  1: '',
+  1: 'src/assets/images/icon-partly-cloudy.webp',
+  2: 'src/assets/images/icon-partly-cloudy.webp',
+  3: 'src/assets/images/icon-overcast.webp',
+
+  // Fog / Mist
+  45: 'src/assets/images/icon-fog.webp',
+  48: 'src/assets/images/icon-fog.webp',
+
+  // Drizzle
+  51: 'src/assets/images/icon-drizzle.webp',
+  53: 'src/assets/images/icon-drizzle.webp',
+  55: 'src/assets/images/icon-drizzle.webp',
+
+  // Freezing Drizzle
+  56: 'src/assets/images/icon-drizzle.webp',
+  57: 'src/assets/images/icon-drizzle.webp',
+
+  // Rain
+  61: 'src/assets/images/icon-rain.webp',
+  63: 'src/assets/images/icon-rain.webp',
+  65: 'src/assets/images/icon-rain.webp',
+
+  // Freezing Rain
+  66: 'src/assets/images/icon-rain.webp',
+  67: 'src/assets/images/icon-rain.webp',
+
+  // Snow
+  71: 'src/assets/images/icon-snow.webp',
+  73: 'src/assets/images/icon-snow.webp',
+  75: 'src/assets/images/icon-snow.webp',
+
+  // Snow Grains
+  77: 'src/assets/images/icon-snow.webp',
+
+  // Rain + Snow (Mixed)
+  80: 'src/assets/images/icon-rain.webp',
+  81: 'src/assets/images/icon-rain.webp',
+  82: 'src/assets/images/icon-rain.webp',
+
+  // Showers
+  85: 'src/assets/images/icon-snow.webp',
+  86: 'src/assets/images/icon-snow.webp',
+
+  // Thunderstorm
+  95: 'src/assets/images/icon-storm.webp',
+  96: 'src/assets/images/icon-storm.webp',
+  99: 'src/assets/images/icon-storm.webp',
 };
