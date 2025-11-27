4a09e0171919c3bc2b71ba1bf95439a99d1e5612
omotolani <omotolanionajoko@gmail.com>
2025-11-27 13:26:49 +0100

    fix(header):fix header position while scrolling

--- src/sections/Header.tsx
+++ src/sections/Header.tsx
@@ -2,7 +2,7 @@ import logo from '../assets/images/logo.svg';
 import UnitsDropdown from '../components/UnitsDropdown';
 const Header = () => {
   return (
-    <header className="flex place-items-center justify-between w-full py-5 md:px-10">
+    <header className="flex place-items-center justify-between w-full py-5 md:px-10 sticky top-0 bg-inherit">
       <span className="">
         <img src={logo} alt="Weather Now logo" className="w-35" />
       </span>
