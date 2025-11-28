437b88b4e6494eb1e958f9a783c9f7a330a1b8ef
omotolani <omotolanionajoko@gmail.com>
2025-11-28 21:57:11 +0100

    feat(loader ui):add loader ui

--- src/components/Atmosphere.tsx
+++ src/components/Atmosphere.tsx
@@ -6,7 +6,7 @@ interface AtmospherePropsTypes {
 }
 const Atmosphere = ({ title, value, unit, space }: AtmospherePropsTypes) => {
   return (
-    <div className="bg-neutral-800 text-neutral-0 py-5 px-2 rounded-2xl font-DM-sans flex flex-col gap-5 w-33 sm:w-45">
+    <div className="bg-neutral-800 text-neutral-0 py-5 px-3 rounded-2xl font-DM-sans flex flex-col gap-5 w-33 sm:w-45">
       <p className="text-lg text-neutral-300 ">{title}</p>
       <h2 className="text-3xl">
         {value}
--- src/components/WeatherCard.tsx
+++ src/components/WeatherCard.tsx
@@ -6,7 +6,7 @@ import { getWeatherIcon } from '../utils/WeatherIcon';
 const WeatherCard = () => {
   const { query } = useSearch();
   const { city, country } = useGeolocation(query);
-  const { time, currentTemp, currentWeatherCode } = useWeather();
+  const { time, currentTemp, currentWeatherCode, loading } = useWeather();
 
   const date = new Date(time);
 
@@ -19,23 +19,32 @@ const WeatherCard = () => {
 
   return (
     <section
-      className="min-h-60 overflow-hidden w-full max-w-70 bg-center bg-cover xxs:max-w-[650px] font-DM-sans text-neutral-0 rounded-xl bg-[url('/bg/bg-today-small.svg')]
-    xs:bg-[url('/bg/bg-today-large.svg')] bg-no-repeat  flex items-centers"
+      className={`min-h-60 overflow-hidden w-full max-w-70 bg-center bg-cover xxs:max-w-[650px] font-DM-sans text-neutral-0 rounded-xl ${
+        loading ? 'bg-neutral-800' : "bg-[url('/bg/bg-today-small.svg')] xs:bg-[url('/bg/bg-today-large.svg')] "
+      }
+     bg-no-repeat  flex items-centers`}
     >
-      <div className=" flex flex-col sm:flex-row items-center justify-center gap-5 w-full font-DM-sans sm:px-10 text-center">
-        <div className="">
-          {city && (
-            <h2 className="capitalize font-medium text-2xl">
-              {city}, {country}
-            </h2>
-          )}
-          <p className="capitalize font-light text-sm">{time && formattedDate}</p>
+      {loading ? (
+        <div className="w-full flex flex-col gap-2 justify-center items-center">
+          <div className="loader"></div>
+          <p>loading...</p>
         </div>
-        <div className="text-7xl lg:text-8xl font-DM-sans  italic flex font-medium  items-center">
-          {currentWeatherCode !== null && <img src={getWeatherIcon(currentWeatherCode)} className="w-25 sm:w-35" />}
-          {currentTemp && <p>{Math.floor(Number(currentTemp))}&deg;</p>}
+      ) : (
+        <div className=" flex flex-col sm:flex-row items-center justify-center gap-5 w-full font-DM-sans sm:px-10 text-center">
+          <div className="">
+            {city && (
+              <h2 className="capitalize font-medium text-2xl">
+                {city}, {country}
+              </h2>
+            )}
+            <p className="capitalize font-light text-sm">{time && formattedDate}</p>
+          </div>
+          <div className="text-7xl lg:text-8xl font-DM-sans  italic flex font-medium  items-center">
+            {currentWeatherCode !== null && <img src={getWeatherIcon(currentWeatherCode)} className="w-25 sm:w-35" />}
+            {currentTemp && <p>{Math.floor(Number(currentTemp))}&deg;</p>}
+          </div>
         </div>
-      </div>
+      )}
     </section>
   );
 };
